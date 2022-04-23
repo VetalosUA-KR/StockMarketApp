1404b64b8d4d4b0a862c70f4fd6e1755b6bd568d
Vitaliy.V <vitaliy.v@rockalab.com>
2022-04-23 11:49:43 +0300

    added mapper for entity and dao, added dao and api

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,20 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apikey: String
+    ): ResponseBody
+
+
+    companion object {
+        const val API_KEY = "ZB9G6P916RZA2H0U"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+
+}
