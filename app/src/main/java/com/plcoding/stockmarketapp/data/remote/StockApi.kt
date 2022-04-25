cb805fcec1c39e0a8b94859601e35a63c643e03a
Vitaliy.V <vitaliy.v@rockalab.com>
2022-04-25 18:22:57 +0300

    added UI and back for detail screen (company info and intraday info) added mapper for those DTO object, and added intraday parser to DI graph

--- app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package com.plcoding.stockmarketapp.data.remote
 
+import com.plcoding.stockmarketapp.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,6 +12,17 @@ interface StockApi {
         @Query("apikey") apikey: String = API_KEY
     ): ResponseBody
 
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?funcion=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): CompanyInfoDto
 
     companion object {
         const val API_KEY = "ZB9G6P916RZA2H0U"
